1e9306b06600495a285ad2c4614daecce0f5bf66
Osipov Konstantin <k.osipov.msk@gmail.com>
2016-08-27 01:06:10 +0300

    Added Benchmark test

---
+++ text-generator_bench_test.go
@@ -0,0 +1,19 @@
+package text_generator
+
+import (
+	"testing"
+)
+
+func BenchmarkGenerateEasyText(b *testing.B) {
+	tg := New()
+	for i := 0; i < b.N; i++ {
+		tg.Generate("Good {morning|day}!")
+	}
+}
+
+func BenchmarkGenerateComplexText(b *testing.B) {
+	tg := New()
+	for i := 0; i < b.N; i++ {
+		tg.Generate("{Good {morning|evening|day}|Goodnight|Hello}, {friend|brother}! {How are you|What's new with you}?")
+	}
+}
